b37753307b5f8ac8eee43a5c8280bb6d9a752be7
shatatarakau <87065842+shatatarakau@users.noreply.github.com>
2026-02-28 23:06:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send("App Running - CI Test");
+    res.send("App Running - Deployed to EC2 successfully");
 });
 
 app.listen(3000, ()=> console.log("Server started"));
